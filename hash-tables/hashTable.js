d0eb1b5925eb2951c29a27f9d004e24cb451a826
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-02 20:33:52 -0400

    added set solution for hash table

--- hash-tables/hashTable.js
+++ hash-tables/hashTable.js
@@ -6,12 +6,34 @@ class HashTable {
     _hash(key){
         let total = 0;
         let weirdPrime = 31;
-        for(let i = 0; i < Math.min(string.length, 100); i++){
+        for(let i = 0; i < Math.min(key.length, 100); i++){
             // this will map each letter to the corresponding position in the alphabet
-            let char = string[i];
+            let char = key[i];
             let value = char.charCodeAt(0) - 96;
             total = (total * weirdPrime + value) % lengthOfArray;
         }
         return total;
     }
+
+    set(key, value){
+        let index = this._hash(key);
+        if(!this.keyMap[index]){
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+        return index;
+    }
+
+    get(key){
+
+    }
 }
+
+// set
+// accepts a key/value pasir
+//  hashes the key
+// stores the key value pair in the hash table via seperate chaining
+
+// get
+// accept a key
+// hashes the key
