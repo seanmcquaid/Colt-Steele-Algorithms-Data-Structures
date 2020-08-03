4dd9bef6e8578342cb5ca3bd711f30b7a9a4ef14
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-02 20:44:18 -0400

    finished hash table section, review from java

--- hash-tables/hashTable.js
+++ hash-tables/hashTable.js
@@ -35,6 +35,34 @@ class HashTable {
         }
         return undefined;
     }
+
+    values(){
+        let valuesArr = [];
+        for(let i = 0; i < this.keyMap.length; i++){
+            if(this.keyMap[i]){
+                for(let j = 0; j < this.keyMap[i].length; j++){
+                    if(valuesArr.includes(this.keyMap[i][j][1])){
+                        valuesArr.push(this.keyMap[i][j][1]);
+                    }
+                }
+            }
+        }
+        return valuesArr;
+    }
+
+    keys(){
+        let keysArr = [];
+        for(let i = 0; i < this.keyMap.length; i++){
+            if(this.keyMap[i]){
+                for(let j = 0; j < this.keyMap[i].length; j++){
+                    if(keysArr.includes(this.keyMap[i][j][0])){
+                        keysArr.push(this.keyMap[i][j][0]);
+                    }
+                }
+            }
+        }
+        return keysArr;
+    }
 }
 
 // set
@@ -45,3 +73,9 @@ class HashTable {
 // get
 // accept a key
 // hashes the key
+
+// keys
+// loops through the hash table aray and return an array of keys in the table
+
+// values
+// loops through the hash table array and returns an array of values in the table
