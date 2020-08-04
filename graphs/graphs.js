8b721711ea46ec62b69a78d6d89a520303f0223a
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-03 21:26:21 -0400

    first notes on graphs

---
+++ graphs/graphs.js
@@ -0,0 +1,19 @@
+// Graphs
+
+// A graph data structure consists of a finite set of nodes, together with a set of unordered pairs
+// for an undirected graph or a set of ordered pairs for a directed graph
+
+// Uses for Graphs
+// Social Networks
+// Location / Mapping
+// Routing Algorithms
+// Visual hierarchy
+// file system structure
+
+// Types of Graphs
+// Vertex - a node
+// Edge - Connection between nodes
+// Undirected - no polarity to the edges - two way connections
+// Directed - represented with arrows - polarity with each edge - one way connections
+// Weighted - has value for each edge
+// Unweighted - has no value for each edge
