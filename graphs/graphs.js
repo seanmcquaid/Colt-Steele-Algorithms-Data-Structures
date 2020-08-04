a4e801500f7c7344d7d306bb9741de9cd3c5fde2
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-03 21:43:25 -0400

    add edge solution

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -28,4 +28,9 @@ class Graph {
             this.adjacencyList[vertex] = [];
         }
     }
+
+    addEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
 }
