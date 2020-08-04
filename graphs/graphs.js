4d7b79fdf8fabae73914f846012b3d9fdb71b25d
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-03 21:45:11 -0400

    added remove edge function

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -33,4 +33,9 @@ class Graph {
         this.adjacencyList[vertex1].push(vertex2);
         this.adjacencyList[vertex2].push(vertex1);
     }
+
+    removeEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1);
+    }
 }
