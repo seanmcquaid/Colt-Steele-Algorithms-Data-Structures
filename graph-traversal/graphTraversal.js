df4ebe1829b4a23fdcc672f771f6e07972aaf782
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-04 20:25:58 -0400

    recursive depth first search

--- graph-traversal/graphTraversal.js
+++ graph-traversal/graphTraversal.js
@@ -5,3 +5,72 @@
 // finding closest matches/recommendations
 // Shortest path problems 
 // GPS Navigation, Solving Mazes and AI (Shortest path to win a game)
+
+// Depth First
+// Explore as far down one track before backtracking
+
+// Recursive DFS
+// DFS(vertex)
+// if vertex is empty - return this
+// add vertex to results list
+// mark vertex as visited
+// for each neighbor in vertex neighbors : 
+// if neighbor is not visited
+// recursively call dfs on neighbor
+
+class Graph {
+    constructor(){
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    removeEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1);
+    }
+
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+
+        delete this.adjacencyList[vertex];
+    }
+
+    depthFirstRecursive(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        function dfs(vertex){
+            if(!vertex){
+                return null;
+            }
+
+            result.push(vertex);
+            visited[vertex] = true;
+
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return depthFirstRecursive(neighbor);
+                }
+            });
+
+        }
+
+        dfs(start);
+
+        return result;
+    }
+}
