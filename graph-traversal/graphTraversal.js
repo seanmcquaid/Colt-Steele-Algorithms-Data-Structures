d48f59862d2a4fe10444cb9e4c59836b88ea08b6
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-04 20:11:50 -0400

    notes on graph traversal

---
+++ graph-traversal/graphTraversal.js
@@ -0,0 +1,7 @@
+// Graph Traversal Use Cases
+
+// Peer to peer networking
+// Web crawlers
+// finding closest matches/recommendations
+// Shortest path problems 
+// GPS Navigation, Solving Mazes and AI (Shortest path to win a game)
