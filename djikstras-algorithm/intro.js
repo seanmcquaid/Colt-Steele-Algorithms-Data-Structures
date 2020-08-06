735e738b28548e5eba91f55f4ba6fd05b56c2b1e
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-05 21:10:19 -0400

    added notes on djikstras algorithm

---
+++ djikstras-algorithm/intro.js
@@ -0,0 +1,11 @@
+// Djikstra's Algorithm
+// Uses Graphs + Priority Queues with Binary Heap
+// One of the most famous and widely used algorithm around
+
+// Finds the shortest path between two vertices on a graph
+
+// Why is it useful?
+// GPS - finding fastest route
+// Network routing - finds open shortest path for data
+// Biology - used to model sthe spread of a virus between humans
+// airlines - same principle as GPS
