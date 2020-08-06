d9a25090f775aed348d9a39090543ffe59bea562
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-05 21:47:15 -0400

    set up initial state for alg

--- dijkstras-algorithm/djkistras.js
+++ dijkstras-algorithm/djkistras.js
@@ -32,4 +32,21 @@ class WeightedGraph {
         this.adjacencyList[vertex1].push({node : vertex2, weight});
         this.adjacencyList[vertex2].push({node : vertex1, weight});
     }
+
+    djikstra(start, finish){
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        
+        for(let vertex in this.adjacencyList){
+             if(vertex === start){
+                 distances[vertex] = 0;
+                 nodes.enqueue(vertex, 0);
+             } else {
+                 distances[vertex] = Infinity;
+                 nodes.enqueue(vertex, Infinity);
+             }
+             previous[vertex] = null;
+        }
+    }
 }
