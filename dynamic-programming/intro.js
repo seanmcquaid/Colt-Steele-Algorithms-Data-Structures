191b52b613f4daabca230f83ad8083053cd5cfc0
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-06 22:27:10 -0400

    started notes on dynamic programming

---
+++ dynamic-programming/intro.js
@@ -0,0 +1,16 @@
+// Dynamic Programming
+// "A method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions."
+
+// It only works on problems with 
+// OPTIMAL SUBSTRUCTURE    &
+// OVERLAPPING SUBPROBLEMS
+
+// OVERLAPPING
+// SUBPROBLEMS
+// A problem is said to have overlapping subproblems if it can be broken down into subproblems which are reused several times
+
+// FIBONACCI SEQUENCE
+// "Every number after the first two is the sum of the two preceding ones"
+
+// OPTIMAL SUBSTRUCTURE
+// A problem is said to have optimal substructure if an optimal solution can be constructed from optimal solutions of its subproblems
