a7a41fc400032fb54ecce8e367d96c511a390462
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-06 22:37:09 -0400

    more notes on dynamic programming

--- dynamic-programming/intro.js
+++ dynamic-programming/intro.js
@@ -14,3 +14,45 @@
 
 // OPTIMAL SUBSTRUCTURE
 // A problem is said to have optimal substructure if an optimal solution can be constructed from optimal solutions of its subproblems
+
+// MEMOIZATION
+// Storing the results of expensive function calls and returning the cached result when the same inputs occur again
+
+// TABULATION
+// Storing the result of a previous result in a "table" (usually an array)
+// Usually done using iteration
+// Better space complexity can be achieved using tabulation
+
+// MEMOIZATION
+// Storing the result of an expensive function
+// Usually done using recursion
+
+// TABULATION
+// Storing the result of a previous result in a "table" (usually an array)
+// Usually done using iteration
+// Better space complexity can be achieved using tabulation
+
+// WHERE IS THIS ACTUALLY USED?
+// Artificial Intelligence
+// Speech Recognition
+// Caching
+// Image Processing
+// Shortest Path Algorithms
+// Much, much more!
+
+// GREEDY ALGORITHMS
+// A greedy algorithm is an algorithmic paradigm that follows the problem solving heuristic of making the locally optimal choice at each stage with the hope of finding a global optimum.
+
+// WHAT?
+// An algorithm that makes the best guess about what the right answer is and tries to solve it that way as quickly as possible!
+
+// WHERE ARE THEY USED?
+// You've seen one already!
+// Remember how Dijkstra's Algorithm works!
+
+// RECAP
+// Dynamic Programming is the idea of breaking down a problem into smaller subproblems - it's hard
+// Optimal substructure is required to use dynamic program and involves figuring out the correct expression to consistently solve subproblems
+// Overlapping subproblems is the second essential part of dynamic programming 
+// Greedy Algorithms are a more aggressive and not always efficient way of solving algorithms
+// Backtracking is quite useful when solving for restrictive conditions with unknown possibilities
